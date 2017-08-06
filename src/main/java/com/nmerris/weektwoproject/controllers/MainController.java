8db8875ff0b3a42d185375fef57b117759af3a92
Nathan Merris <joorge.jetson@gmail.com>
2017-08-06 07:50:07 -0400

    addresume form validation is all good except still can't get DateTimeFormat annotation to work, changed all @NotNull to @NotEmpty also

--- src/main/java/com/nmerris/weektwoproject/controllers/MainController.java
+++ src/main/java/com/nmerris/weektwoproject/controllers/MainController.java
@@ -1,8 +1,6 @@
 package com.nmerris.weektwoproject.controllers;
 
-
 import javax.validation.Valid;
-
 import com.nmerris.weektwoproject.models.Resume;
 import com.nmerris.weektwoproject.repositories.ResumeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.ResourceBundle;
 
-import static com.nmerris.weektwoproject.utilities.Utilities.setDefaultLocaleToUsSpanish;
-
-// define this class as a 'Spring bean'
 @Controller
 public class MainController
 {
@@ -27,15 +21,9 @@ public class MainController
 
 
     @GetMapping("/index")
-    public String indexPageGet(Model model)
+    public String indexPageGet()
     {
-//        // TODO add a link in index that changes the language to us spanish
-//        ResourceBundle rb = setDefaultLocaleToUsSpanish();
-//
-//        model.addAttribute("indexTitle",rb.getString("indexTitle"));
-//        model.addAttribute("welcomeMessage",rb.getString("welcomeMessage"));
-//        model.addAttribute("addResumeLinkText",rb.getString("addResumeLinkText"));
-//        model.addAttribute("displayAllResumesLinkText",rb.getString("displayAllResumesLinkText"));
+        // TODO add a link in index that changes the language to us spanish
 
         return "index";
     }
@@ -44,37 +32,18 @@ public class MainController
     @GetMapping("/addresume")
     public String addBook(Model model)
     {
-//        ResourceBundle rb = setDefaultLocaleToUsSpanish();
-
-
         model.addAttribute("newResume", new Resume());
-
-        // add all generic attributes to model
-//        addGenericAttributesToAddBookModel(model, rb);
 
         return "addresume";
     }
 
 
-
-
     @PostMapping("/addresume")
     public String postProduct(@Valid @ModelAttribute("newResume") Resume resume, BindingResult bindingResult, Model model)
     {
-        ResourceBundle rb = setDefaultLocaleToUsSpanish();
 
         if (bindingResult.hasErrors()) {
             System.out.println("**************************************** VALIDATION ERROR ************************");
-
-            addGenericAttributesToAddBookModel(model, rb);
-
-            // here we need to add error specific attributes
-            // handles both first and last name errors (same error msg in each case)
-            model.addAttribute("nameFormError", rb.getString("nameFormError"));
-            model.addAttribute("emailFormError", rb.getString("emailFormError"));
-            model.addAttribute("dateFormError", rb.getString("dateFormError"));
-
-
 
             return "addresume";
         }
@@ -93,20 +62,6 @@ public class MainController
         Iterable<Resume> resumeList = resumeRepository.findAll();
         model.addAttribute("allResumes", resumeList);
         return "displayallresumes";
-    }
-
-
-    // many of the text in the addresume page is the same for both GET and POST routes
-    // don't want to repeat myself, so extracted them to this method
-    private void addGenericAttributesToAddBookModel(Model model, ResourceBundle rb) {
-        model.addAttribute("addResumeHeading", rb.getString("addResumeHeading"));
-        model.addAttribute("addResumeTitle", rb.getString("addResumeTitle"));
-        model.addAttribute("firstNameFormTitle", rb.getString("firstNameFormTitle"));
-        model.addAttribute("lastNameFormTitle", rb.getString("lastNameFormTitle"));
-        model.addAttribute("emailFormTitle", rb.getString("emailFormTitle"));
-        model.addAttribute("orgFormTitle", rb.getString("orgFormTitle"));
-        model.addAttribute("dateStartFormTitle", rb.getString("dateStartFormTitle"));
-        model.addAttribute("dateEndFormTitle", rb.getString("dateEndFormTitle"));
     }
 
 
--- src/main/java/com/nmerris/weektwoproject/models/Resume.java
+++ src/main/java/com/nmerris/weektwoproject/models/Resume.java
@@ -2,17 +2,17 @@ package com.nmerris.weektwoproject.models;
 
 import com.sun.istack.internal.NotNull;
 import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import javax.validation.constraints.Size;
-import java.util.Date;
 
 @Entity
 public class Resume {
 
     // the pattern to validate for date entries
-    private static final String DATE_PATTERN = "MM/dd/yyyy";
+    private static final String DATE_PATTERN = "yyyy-MM-dd";
 
 
     // TODO: id is not auto incrementing, must fix this.. actually changing application.properties file fixes this
@@ -21,12 +21,10 @@ public class Resume {
     private long id;
 
     // suppress the default error message because we want to use our own locale specific message
-    @NotNull
-    @Size(min = 1, max = 40, message = "")
+    @NotEmpty
     private String nameFirst;
 
-    @NotNull
-    @Size(min = 1, max = 40, message = "")
+    @NotEmpty
     private String nameLast;
 
     // email can be null, but if user enters anything it will be validated
@@ -37,10 +35,9 @@ public class Resume {
     private String organization;
 
     // require the date be of a given format, ie 07/04/1776 = July 4th, 1776
-    @NotNull
-    @Temporal(TemporalType.DATE)
+    @NotEmpty
     @DateTimeFormat(pattern = DATE_PATTERN)
-    private Date dateStart;
+    private String dateStart;
 
     // ok to be null, if so assume currently employed
     // but if user enters anything, it will be validated
@@ -66,11 +63,11 @@ public class Resume {
         this.nameLast = nameLast;
     }
 
-    public Date getDateStart() {
+    public String getDateStart() {
         return dateStart;
     }
 
-    public void setDateStart(Date dateStart) {
+    public void setDateStart(String dateStart) {
         this.dateStart = dateStart;
     }
 
